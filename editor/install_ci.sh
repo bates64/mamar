12d5ed00585f347b87b4a39f580fbda45d4d7b98
Alex Bates <hi@imalex.xyz>
2020-11-28 13:53:34 +0000

    depend on wasm-pack externally

--- editor/install_ci.sh
+++ editor/install_ci.sh
@@ -1,6 +1,7 @@
-# rust nightly
+# rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y \
     --profile minimal \
     --target wasm32-unknown-unknown
 
 npm ci
+npm i wasm-pack
