c4795d9836ca20eb6e7d6769790351299fec59cf
Alex Bates <hi@imalex.xyz>
2021-04-15 11:50:35 +0100

    use UiState enum

--- mamar/src/interface/state.rs
+++ mamar/src/interface/state.rs
@@ -19,12 +19,17 @@ pub struct Document {
     pub bgm: Bgm,
     pub path: PathBuf,
 
-    selected_segment_idx: u8,
-    selected_track_subseg_idx: u8,
+    ui_state: UiState,
+}
 
-    viewing_seg_list: bool,
-
-    track_list_interface: TrackListInterface,
+#[derive(Clone)]
+enum UiState {
+    Overview {
+        selected_segment_idx: u8,
+        track_list_interface: TrackListInterface,
+        viewing_seg_list: bool,
+        selected_track_subseg_idx: u8,
+    },
 }
 
 // Change of anything other than self.bgm should not be considered a History-changing action.
@@ -70,10 +75,12 @@ impl Document {
         Ok(Some(Document {
             bgm,
             path,
-            selected_segment_idx: 0,
-            selected_track_subseg_idx: 0,
-            viewing_seg_list: false,
-            track_list_interface: TrackListInterface::new(),
+            ui_state: UiState::Overview {
+                selected_segment_idx: 0,
+                track_list_interface: TrackListInterface::new(),
+                viewing_seg_list: false,
+                selected_track_subseg_idx: 0,
+            },
         }))
     }
 
@@ -142,120 +149,128 @@ impl Document {
     }
 
     pub fn update(&mut self, ui: &mut imui_glium::UiFrame<'_>) {
-        ui.vbox(0, |ui| {
-            ui.hbox(0, |ui| {
-                let segments = &self.bgm.segments;
-                if range_select(
-                    ui,
-                    0,
-                    0..self.bgm.segments.len() as isize,
-                    1,
-                    &mut self.selected_segment_idx,
-                    |i| {
-                        if let Some(seg) = &segments[*i as usize] {
-                            seg.name.to_owned()
-                        } else {
-                            String::from("(no data)")
+        let bgm = &mut self.bgm;
+        match &mut self.ui_state {
+            UiState::Overview {
+                selected_segment_idx,
+                track_list_interface,
+                viewing_seg_list,
+                selected_track_subseg_idx,
+            } => {
+                ui.vbox(0, |ui| {
+                    ui.hbox(0, |ui| {
+                        if range_select(
+                            ui,
+                            0,
+                            0..bgm.segments.len() as isize,
+                            1,
+                            selected_segment_idx,
+                            |i| {
+                                if let Some(seg) = &bgm.segments[*i as usize] {
+                                    seg.name.to_owned()
+                                } else {
+                                    String::from("(no data)")
+                                }
+                            },
+                        ) {
+                            *track_list_interface = TrackListInterface::new();
                         }
-                    },
-                ) {
-                    self.track_list_interface = TrackListInterface::new();
-                }
 
-                if ui.button(1, "Edit").clicked() {
-                    self.viewing_seg_list = true;
-                }
+                        if ui.button(1, "Edit").clicked() {
+                            *viewing_seg_list = true;
+                        }
 
-                if self.viewing_seg_list {
-                    ui.modal(2, true, (300.0, 300.0), |ui| {
-                        ui.text(0, "Song Variations").center_x();
-                        ui.pad(1, 30.0);
-                        ui.known_size(2, 300.0, 32.0 * self.bgm.segments.len() as f32, |ui| {
-                            ui.vbox(0, |ui| {
-                                let mut swap = None;
+                        if *viewing_seg_list {
+                            ui.modal(2, true, (300.0, 300.0), |ui| {
+                                ui.text(0, "Song Variations").center_x();
+                                ui.pad(1, 30.0);
+                                ui.known_size(2, 300.0, 32.0 * bgm.segments.len() as f32, |ui| {
+                                    ui.vbox(0, |ui| {
+                                        let mut swap = None;
 
-                                for (i, seg) in self.bgm.segments.iter().enumerate() {
-                                    ui.hbox(i as u8, |ui| {
-                                        ui.known_size(0, 220.0, 32.0,|ui| {
-                                            ui.text(0, if let Some(seg) = seg {
-                                                &seg.name
-                                            } else {
-                                                "(no data)"
-                                            }).center_y();
-                                        });
+                                        for (i, seg) in bgm.segments.iter().enumerate() {
+                                            ui.hbox(i as u8, |ui| {
+                                                ui.known_size(0, 220.0, 32.0,|ui| {
+                                                    ui.text(0, if let Some(seg) = seg {
+                                                        &seg.name
+                                                    } else {
+                                                        "(no data)"
+                                                    }).center_y();
+                                                });
 
-                                        if ui.button(1, "^").with_width(32.0).clicked() {
-                                            if i > 0 {
-                                                swap = Some((i, i - 1));
-                                            }
+                                                if ui.button(1, "^").with_width(32.0).clicked() {
+                                                    if i > 0 {
+                                                        swap = Some((i, i - 1));
+                                                    }
+                                                }
+                                                if ui.button(2, "v").with_width(32.0).clicked() {
+                                                    if i < bgm.segments.len() - 1 {
+                                                        swap = Some((i, i + 1));
+                                                    }
+                                                }
+                                            });
                                         }
-                                        if ui.button(2, "v").with_width(32.0).clicked() {
-                                            if i < self.bgm.segments.len() - 1 {
-                                                swap = Some((i, i + 1));
-                                            }
+
+                                        if let Some((a, b)) = swap {
+                                            bgm.segments.swap(a, b);
                                         }
                                     });
+                                });
+                                ui.pad(3, 50.0);
+                                if ui.button(4, "Close").clicked() {
+                                    *viewing_seg_list = false;
                                 }
-
-                                if let Some((a, b)) = swap {
-                                    self.bgm.segments.swap(a, b);
-                                }
-                            });
-                        });
-                        ui.pad(3, 50.0);
-                        if ui.button(4, "Close").clicked() {
-                            self.viewing_seg_list = false;
+                            })
                         }
-                    })
-                }
-            });
+                    });
 
-            ui.pad(3, 5.0);
+                    ui.pad(3, 5.0);
 
-            if let Some(segment) = self.bgm.segments[self.selected_segment_idx as usize].as_mut() {
-                let track_subsegs: Vec<(&u8, &TrackListId)> = segment.subsegments
-                    .iter()
-                    .filter_map(|s| match s {
-                        Subsegment::Tracks { flags, track_list } => Some((flags, track_list)),
-                        Subsegment::Unknown { .. } => None,
-                    })
-                    .collect();
+                    if let Some(segment) = bgm.segments[*selected_segment_idx as usize].as_mut() {
+                        let track_subsegs: Vec<(&u8, &TrackListId)> = segment.subsegments
+                            .iter()
+                            .filter_map(|s| match s {
+                                Subsegment::Tracks { flags, track_list } => Some((flags, track_list)),
+                                Subsegment::Unknown { .. } => None,
+                            })
+                            .collect();
 
-                let range = 0..track_subsegs.len() as isize;
+                        let range = 0..track_subsegs.len() as isize;
 
-                if !range.contains(&(self.selected_track_subseg_idx as isize)) {
-                    self.selected_track_subseg_idx = 0;
-                }
+                        if !range.contains(&(*selected_track_subseg_idx as isize)) {
+                            *selected_track_subseg_idx = 0;
+                        }
 
-                let track_lists = &self.bgm.track_lists;
-                if range_select(
-                    ui,
-                    4,
-                    range,
-                    1,
-                    &mut self.selected_track_subseg_idx,
-                    |v| {
-                        let (_, track_list_id) = track_subsegs[*v as usize];
-                        let track_list = &track_lists[track_list_id];
-                        track_list.name.to_owned()
-                    },
-                ) {
-                    self.track_list_interface = TrackListInterface::new();
-                }
+                        let track_lists = &bgm.track_lists;
+                        if range_select(
+                            ui,
+                            4,
+                            range,
+                            1,
+                            &mut *selected_track_subseg_idx,
+                            |v| {
+                                let (_, track_list_id) = track_subsegs[*v as usize];
+                                let track_list = &track_lists[track_list_id];
+                                track_list.name.to_owned()
+                            },
+                        ) {
+                            *track_list_interface = TrackListInterface::new();
+                        }
 
-                ui.pad(7, 10.0);
+                        ui.pad(7, 10.0);
 
-                let (flags, track_list_id) = track_subsegs[self.selected_track_subseg_idx as usize];
-                let track_list = self.bgm.track_lists.get_mut(track_list_id).unwrap();
-                let track_list_interface = &mut self.track_list_interface;
-                let voices = &mut self.bgm.voices;
+                        let (flags, track_list_id) = track_subsegs[*selected_track_subseg_idx as usize];
+                        let track_list = bgm.track_lists.get_mut(track_list_id).unwrap();
+                        let voices = &mut bgm.voices;
 
-                ui.text(8, format!("Flags: {:08X}", flags));
-                ui.pad(9, 10.0);
-                ui.vbox(10, |ui| {
-                    track_list_interface.update(ui, track_list, voices);
+                        ui.text(8, format!("Flags: {:08X}", flags));
+                        ui.pad(9, 10.0);
+                        ui.vbox(10, |ui| {
+                            track_list_interface.update(ui, track_list, voices);
+                        });
+                    }
                 });
             }
-        });
+        }
     }
 }
