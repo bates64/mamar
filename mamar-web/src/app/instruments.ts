3d6b2ee2e54a14fef75fcc4f26c4c810fe5a2c53
Alex Bates <alex@nanaian.town>
2022-10-16 17:22:48 +0100

    update instruments list per review

--- mamar-web/src/app/instruments.ts
+++ mamar-web/src/app/instruments.ts
@@ -3,114 +3,148 @@ export const categories = [
         name: "Keyboard",
         instruments: [
             { bank: 0x03, patch: 0x0E, name: "Old School Piano" },
+
             { bank: 0x03, patch: 0x0F, name: "Bright Piano" },
+
             { bank: 0x03, patch: 0x20, name: "Electric Piano 1A" },
             { bank: 0x03, patch: 0x21, name: "Electric Piano 1B" },
             { bank: 0x03, patch: 0x22, name: "Electric Piano 2" },
-            { bank: 0x03, patch: 0x23, name: "Acoustic Piano 1" },
-            { bank: 0x03, patch: 0x24, name: "Acoustic Piano 2" },
-            { bank: 0x03, patch: 0x00, name: "Marimba 1" },
-            { bank: 0x03, patch: 0x01, name: "Marimba 2" },
-            { bank: 0x03, patch: 0x02, name: "Marimba 3" },
-            { bank: 0x03, patch: 0x03, name: "Xylophone 1" },
-            { bank: 0x03, patch: 0x04, name: "Xylophone 2" },
-            { bank: 0x03, patch: 0x05, name: "Xylophone 3" },
-            { bank: 0x03, patch: 0x0F, name: "Xylophone 4" },
-            { bank: 0x03, patch: 0x06, name: "Vibraphone 1" },
-            { bank: 0x03, patch: 0x07, name: "Vibraphone 2" },
-            { bank: 0x03, patch: 0x08, name: "Vibraphone 3" },
-            { bank: 0x03, patch: 0x09, name: "Celesta 1" },
-            { bank: 0x03, patch: 0x0A, name: "Celesta 2" },
-            { bank: 0x03, patch: 0x25, name: "Music Box 1" },
-            { bank: 0x03, patch: 0x26, name: "Music Box 2" },
-            { bank: 0x03, patch: 0x46, name: "Kalimba" },
-            { bank: 0x03, patch: 0x4D, name: "Percussive Organ 1" },
-            { bank: 0x03, patch: 0x4E, name: "Drawbar Organ 1" },
-            { bank: 0x03, patch: 0x4F, name: "Drawbar Organ 2" },
-            { bank: 0x03, patch: 0x54, name: "Percussive Organ 2" },
-            { bank: 0x03, patch: 0x5E, name: "Vibraphone 4" },
-            { bank: 0x03, patch: 0x5F, name: "Vibraphone 5" },
-            { bank: 0x03, patch: 0x60, name: "Harpsichord 1" },
-            { bank: 0x03, patch: 0x61, name: "Harpsichord 2" },
-            { bank: 0x03, patch: 0x62, name: "Harpsichord 3" },
-            { bank: 0x03, patch: 0x63, name: "Rock Organ 1" },
-            { bank: 0x03, patch: 0x64, name: "Rock Organ 2" },
             { bank: 0x03, patch: 0x75, name: "Electric Piano 3" },
             { bank: 0x03, patch: 0x76, name: "Electric Piano 4" },
             { bank: 0x03, patch: 0x77, name: "Electric Piano 5" },
             { bank: 0x03, patch: 0x78, name: "Electric Piano 6" },
-            { bank: 0x03, patch: 0x7B, name: "Celesta 3" },
+
+            { bank: 0x03, patch: 0x23, name: "Acoustic Piano 1" },
+            { bank: 0x03, patch: 0x24, name: "Acoustic Piano 2" },
+
+            { bank: 0x03, patch: 0x00, name: "Marimba 1" },
+            { bank: 0x03, patch: 0x01, name: "Marimba 2" },
+            { bank: 0x03, patch: 0x02, name: "Marimba 3" },
+
+            { bank: 0x03, patch: 0x03, name: "Xylophone 1" },
+            { bank: 0x03, patch: 0x04, name: "Xylophone 2" },
+            { bank: 0x03, patch: 0x05, name: "Xylophone 3" },
+            { bank: 0x03, patch: 0x0F, name: "Xylophone 4" },
+
+            { bank: 0x03, patch: 0x06, name: "Vibraphone 1" },
+            { bank: 0x03, patch: 0x07, name: "Vibraphone 2" },
+            { bank: 0x03, patch: 0x08, name: "Vibraphone 3" },
+            { bank: 0x03, patch: 0x5E, name: "Vibraphone 4" },
+            { bank: 0x03, patch: 0x5F, name: "Vibraphone 5" },
             { bank: 0x03, patch: 0x7A, name: "Vibraphone 6" },
+
+            { bank: 0x03, patch: 0x09, name: "Celesta 1" },
+            { bank: 0x03, patch: 0x0A, name: "Celesta 2" },
+            { bank: 0x03, patch: 0x7B, name: "Celesta 3" },
+
+            { bank: 0x03, patch: 0x46, name: "Kalimba" },
+
+            { bank: 0x03, patch: 0x4D, name: "Percussive Organ 1" },
+            { bank: 0x03, patch: 0x54, name: "Percussive Organ 2" },
+
+            { bank: 0x03, patch: 0x4E, name: "Drawbar Organ 1" },
+            { bank: 0x03, patch: 0x4F, name: "Drawbar Organ 2" },
+
+            { bank: 0x03, patch: 0x63, name: "Rock Organ 1" },
+            { bank: 0x03, patch: 0x64, name: "Rock Organ 2" },
+
+            { bank: 0x03, patch: 0xA3, name: "Church Organ" },
+
+            { bank: 0x03, patch: 0x60, name: "Harpsichord 1" },
+            { bank: 0x03, patch: 0x61, name: "Harpsichord 2" },
+            { bank: 0x03, patch: 0x62, name: "Harpsichord 3" },
+
             { bank: 0x03, patch: 0x80, name: "Glockenspiel 1" },
             { bank: 0x03, patch: 0x81, name: "Glockenspiel 2" },
+
             { bank: 0x03, patch: 0x73, name: "Sawtooth Synth" },
-            { bank: 0x03, patch: 0x74, name: "Synth(?)" },
-            { bank: 0x03, patch: 0x79, name: "Music Box 3" },
-            { bank: 0x03, patch: 0x84, name: "Beep" },
-            { bank: 0x03, patch: 0x85, name: "Clean Beep" },
+            { bank: 0x03, patch: 0x74, name: "Mystery Synth" },
+
             { bank: 0x03, patch: 0x87, name: "Kalimba 2" },
             { bank: 0x03, patch: 0x8C, name: "Vibraphone 7" },
+
+            { bank: 0x03, patch: 0x25, name: "Music Box 1" },
+            { bank: 0x03, patch: 0x26, name: "Music Box 2" },
+            { bank: 0x03, patch: 0x79, name: "Music Box 3" },
             { bank: 0x03, patch: 0x8E, name: "Music Box 4" },
             { bank: 0x03, patch: 0x8F, name: "Music Box 5" },
+            { bank: 0x03, patch: 0xA0, name: "Music Box 6" },
+            { bank: 0x03, patch: 0xA1, name: "Music Box 7" },
+
             { bank: 0x03, patch: 0x91, name: "Harsh Piano" },
             { bank: 0x03, patch: 0x92, name: "Harpsichord 4" },
             { bank: 0x03, patch: 0x93, name: "Harpsichord 5" },
-            { bank: 0x03, patch: 0xA0, name: "Music Box 6" },
-            { bank: 0x03, patch: 0xA1, name: "Music Box 7" },
-            { bank: 0x03, patch: 0xA3, name: "Church Organ" },
+
+            { bank: 0x03, patch: 0x89, name: "Accordion 1" },
+            { bank: 0x03, patch: 0x8A, name: "Accordion 2" },
+            { bank: 0x03, patch: 0x8B, name: "Accordion 3" },
         ],
     },
     {
         name: "String",
         instruments: [
             { bank: 0x03, patch: 0x10, name: "Cello" },
+
             { bank: 0x03, patch: 0x11, name: "Viola" },
+
             { bank: 0x03, patch: 0x12, name: "Violin 1" },
             { bank: 0x03, patch: 0x13, name: "Violin 2" },
+
             { bank: 0x03, patch: 0x14, name: "Pizzicato Strings 1A" },
             { bank: 0x03, patch: 0x15, name: "Pizzicato Strings 1B" },
             { bank: 0x03, patch: 0x16, name: "Pizzicato Strings 2A" },
             { bank: 0x03, patch: 0x17, name: "Pizzicato Strings 2B" },
+
             { bank: 0x03, patch: 0x18, name: "String Ensemble" },
+
             { bank: 0x03, patch: 0x19, name: "Synth String 1" },
             { bank: 0x03, patch: 0x1A, name: "Synth String 2" },
+
             { bank: 0x03, patch: 0x29, name: "Acoustic Guitar 1A" },
             { bank: 0x03, patch: 0x2A, name: "Acoustic Guitar 1B" },
             { bank: 0x03, patch: 0x2B, name: "Acoustic Guitar 2A" },
             { bank: 0x03, patch: 0x2C, name: "Acoustic Guitar 2B" },
-            { bank: 0x03, patch: 0x2F, name: "Synth Bass 1" },
-            { bank: 0x03, patch: 0x40, name: "Acoustic Bass 1" },
-            { bank: 0x03, patch: 0x41, name: "Acoustic Bass 2" },
+
             { bank: 0x03, patch: 0x44, name: "Overdriven Guitar 1" },
             { bank: 0x03, patch: 0x45, name: "Overdriven Guitar 2" },
+
             { bank: 0x03, patch: 0x52, name: "Guitar Harmonics 1" },
             { bank: 0x03, patch: 0x53, name: "Guitar Harmonics 2" },
+
+            { bank: 0x03, patch: 0x2F, name: "Synth Bass 1" },
+            { bank: 0x03, patch: 0x68, name: "Synth Bass 2" },
+            { bank: 0x03, patch: 0x69, name: "Synth Bass 3" },
+            { bank: 0x03, patch: 0x96, name: "Synth Bass 4" },
+            { bank: 0x03, patch: 0x97, name: "Synth Bass 5" },
+            { bank: 0x03, patch: 0x65, name: "Muted Synth Bass" },
+
+            { bank: 0x03, patch: 0x40, name: "Acoustic Bass 1" },
+            { bank: 0x03, patch: 0x41, name: "Acoustic Bass 2" },
+
             { bank: 0x03, patch: 0x5C, name: "Electric Bass 1" },
             { bank: 0x03, patch: 0x5D, name: "Electric Bass 2" },
-            { bank: 0x03, patch: 0x65, name: "Muted Synth Bass" },
+
+            { bank: 0x03, patch: 0x71, name: "Slap Bass" },
+
+            { bank: 0x03, patch: 0x7C, name: "Plucked Electric Bass" },
+
             { bank: 0x03, patch: 0x55, name: "Sitar 1" },
             { bank: 0x03, patch: 0x66, name: "Sitar 2" },
             { bank: 0x03, patch: 0x67, name: "Sitar 3" },
-            { bank: 0x03, patch: 0x68, name: "Synth Bass 2" },
-            { bank: 0x03, patch: 0x69, name: "Synth Bass 3" },
-            { bank: 0x03, patch: 0x71, name: "Slap Bass" },
-            { bank: 0x03, patch: 0x7C, name: "Plucked Electric Bass" },
+            { bank: 0x03, patch: 0x86, name: "Sitar 4" },
+
             { bank: 0x03, patch: 0x82, name: "Dulcimer 1" },
             { bank: 0x03, patch: 0x83, name: "Dulcimer 2" },
-            { bank: 0x03, patch: 0x86, name: "Sitar 4" },
-            { bank: 0x03, patch: 0x8D, name: "Distortion Strings" },
-            { bank: 0x03, patch: 0x90, name: "Harp" },
-            { bank: 0x03, patch: 0x96, name: "Synth Bass 4" },
-            { bank: 0x03, patch: 0x97, name: "Synth Bass 5" },
 
+            { bank: 0x03, patch: 0x8D, name: "Distortion Strings" },
+
+            { bank: 0x03, patch: 0x90, name: "Harp" },
         ],
     },
     {
         name: "Wind",
         instruments: [
-            { bank: 0x03, patch: 0x0B, name: "Huff n' Puff Synth 1" },
-            { bank: 0x03, patch: 0x0C, name: "Huff n' Puff Synth 2" },
-            { bank: 0x03, patch: 0x0D, name: "Huff n' Puff Synth 3" },
+
             { bank: 0x03, patch: 0x1B, name: "Synth Flute" },
             { bank: 0x03, patch: 0x2E, name: "English Horn 2" },
             { bank: 0x03, patch: 0x30, name: "French Horn" },
@@ -118,14 +152,23 @@ export const categories = [
             { bank: 0x03, patch: 0x32, name: "Tuba 2" },
             { bank: 0x03, patch: 0x33, name: "Trombone 1" },
             { bank: 0x03, patch: 0x34, name: "Trombone 2" },
+
             { bank: 0x03, patch: 0x35, name: "Bassoon 1" },
             { bank: 0x03, patch: 0x36, name: "Bassoon 2" },
             { bank: 0x03, patch: 0x37, name: "Bassoon 3" },
+
             { bank: 0x03, patch: 0x38, name: "Clarinet" },
+
             { bank: 0x03, patch: 0x39, name: "Alto Sax" },
+            { bank: 0x03, patch: 0x94, name: "Tenor Sax 1" },
+            { bank: 0x03, patch: 0x95, name: "Tenor Sax 2" },
+            { bank: 0x03, patch: 0x56, name: "Baritone Sax 1" },
+            { bank: 0x03, patch: 0x57, name: "Baritone Sax 2" },
+
             { bank: 0x03, patch: 0x3A, name: "Oboe 1A" },
             { bank: 0x03, patch: 0x3B, name: "Oboe 1B" },
             { bank: 0x03, patch: 0x3C, name: "Oboe 2" },
+
             { bank: 0x03, patch: 0x3D, name: "Muted Flute" },
             { bank: 0x03, patch: 0x3E, name: "Koopa Bros Synth A" },
             { bank: 0x03, patch: 0x3F, name: "Koopa Bros Synth B" },
@@ -136,37 +179,35 @@ export const categories = [
             { bank: 0x03, patch: 0x49, name: "Flute 3" },
             { bank: 0x03, patch: 0x50, name: "Muted Trumpet 1" },
             { bank: 0x03, patch: 0x51, name: "Muted Trumpet 2" },
-            { bank: 0x03, patch: 0x56, name: "Bari Sax 1" },
-            { bank: 0x03, patch: 0x57, name: "Bari Sax 2" },
+
             { bank: 0x03, patch: 0x58, name: "Muted Trumpet 3" },
             { bank: 0x03, patch: 0x6C, name: "Whistle 1" },
             { bank: 0x03, patch: 0x6D, name: "Whistle 2" },
             { bank: 0x03, patch: 0x6E, name: "Blown Bottle 1" },
             { bank: 0x03, patch: 0x6F, name: "Blown Bottle 2" },
+
             { bank: 0x03, patch: 0x6A, name: "Synth Brass 3" },
             { bank: 0x03, patch: 0x6B, name: "Synth Brass 4" },
+
             { bank: 0x03, patch: 0x70, name: "Shooting Star Pad" },
+
             { bank: 0x03, patch: 0x7D, name: "Whistle 3" },
-            { bank: 0x03, patch: 0x88, name: "Orchestra Hit" },
-            { bank: 0x03, patch: 0x94, name: "Tenor Sax 1" },
-            { bank: 0x03, patch: 0x95, name: "Tenor Sax 2" },
-        ],
-    },
-    {
-        name: "Bass",
-        instruments: [
-            { bank: 0x03, patch: 0x2F, name: "Synth Bass 1" },
+
+            { bank: 0x03, patch: 0x0B, name: "Huff n' Puff Synth 1" },
+            { bank: 0x03, patch: 0x0C, name: "Huff n' Puff Synth 2" },
+            { bank: 0x03, patch: 0x0D, name: "Huff n' Puff Synth 3" },
         ],
     },
     {
         name: "Voice",
         instruments: [
-            { bank: 0x03, patch: 0x59, name: "Choir 1A [Lead 6 (voice)]" },
-            { bank: 0x03, patch: 0x5A, name: "Choir 1B [Lead 6 (voice)]" },
+            { bank: 0x03, patch: 0x59, name: "Choir 1A" },
+            { bank: 0x03, patch: 0x5A, name: "Choir 1B" },
             { bank: 0x03, patch: 0x5B, name: "Choir 2" },
+
+            { bank: 0x03, patch: 0xA5, name: "Synth Voice" },
             { bank: 0x03, patch: 0x7E, name: "Harmonized Synth Voice 1" },
             { bank: 0x03, patch: 0x7F, name: "Harmonized Synth Voice 2" },
-            { bank: 0x03, patch: 0xA5, name: "Synth Voice" },
         ],
     },
     {
@@ -176,28 +217,32 @@ export const categories = [
             { bank: 0x03, patch: 0x1D, name: "Timpani 1B" },
             { bank: 0x03, patch: 0x1E, name: "Timpani 2A" },
             { bank: 0x03, patch: 0x1F, name: "Timpani 2B" },
+
             { bank: 0x03, patch: 0x4A, name: "Steel Drum 1" },
             { bank: 0x03, patch: 0x4B, name: "Steel Drum 2" },
             { bank: 0x03, patch: 0x4C, name: "Steel Drum 3" },
+
             { bank: 0x03, patch: 0x72, name: "Tubular Bells" },
-            { bank: 0x03, patch: 0xA2, name: "Plucked Percussion (?)" },
+
+            { bank: 0x03, patch: 0xA2, name: "Plucked Percussion" },
+
             { bank: 0x03, patch: 0xA4, name: "Reverse Cymbal" },
+
             { bank: 0x03, patch: 0xE4, name: "Woodblock" },
         ],
     },
     {
-        name: "Unsure",
+        name: "Other",
         instruments: [
-            // are accordions keyboards?
-            { bank: 0x03, patch: 0x89, name: "Accordion 1" },
-            { bank: 0x03, patch: 0x8A, name: "Accordion 2" },
-            { bank: 0x03, patch: 0x8B, name: "Accordion 3" },
+            { bank: 0x03, patch: 0x88, name: "Orchestra Hit" },
 
-            // these are just animal sounds
             { bank: 0x03, patch: 0x98, name: "Mosquito" },
-            { bank: 0x03, patch: 0x99, name: "Cat [Lead 8 (bass + lead)]" },
+            { bank: 0x03, patch: 0x99, name: "Cat" },
+
+            { bank: 0x03, patch: 0x84, name: "Beep" },
+            { bank: 0x03, patch: 0x85, name: "Clean Beep" },
         ],
-    }
+    },
 ]
 
 export function getName(bank: number, patch: number): string {
